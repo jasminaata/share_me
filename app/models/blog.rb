9462d02fec499df0c4119223cb294d61e7373f2f
Jasmina Atanassova <jasminaata@gmail.com>
2013-12-01 11:44:51 +0000

    Adding comments model and associations

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,4 +2,5 @@ class Blog < ActiveRecord::Base
   validates :title, presence: true
   validates :body, presence: true, uniqueness: true
   belongs_to :category
+  has_many :comments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131129112226) do
+ActiveRecord::Schema.define(version: 20131201114317) do
 
   create_table "admins", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -46,6 +46,17 @@ ActiveRecord::Schema.define(version: 20131129112226) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "comments", force: true do |t|
+    t.text     "body"
+    t.integer  "user_id"
+    t.integer  "blog_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["blog_id"], name: "index_comments_on_blog_id"
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "",    null: false
