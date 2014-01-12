4a9b45e9c2fcf35f33bf97f5f53a1fcc24b7e46d
Jasmina Atanassova <jasminaata@gmail.com>
2014-01-12 16:15:07 +0000

    Changing db to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,23 +13,8 @@
 
 ActiveRecord::Schema.define(version: 20131210154409) do
 
-  create_table "admins", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "admins", ["email"], name: "index_admins_on_email", unique: true
-  add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "blogs", force: true do |t|
     t.string   "title"
@@ -40,7 +25,7 @@ ActiveRecord::Schema.define(version: 20131210154409) do
     t.string   "image"
   end
 
-  add_index "blogs", ["category_id"], name: "index_blogs_on_category_id"
+  add_index "blogs", ["category_id"], name: "index_blogs_on_category_id", using: :btree
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -56,8 +41,8 @@ ActiveRecord::Schema.define(version: 20131210154409) do
     t.datetime "updated_at"
   end
 
-  add_index "comments", ["blog_id"], name: "index_comments_on_blog_id"
-  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
+  add_index "comments", ["blog_id"], name: "index_comments_on_blog_id", using: :btree
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "",    null: false
@@ -75,7 +60,7 @@ ActiveRecord::Schema.define(version: 20131210154409) do
     t.boolean  "admin",                  default: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
