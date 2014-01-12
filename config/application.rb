af187f1bbfaba99cf2eeb0f4bb38774ff7dfc21b
Jasmina Atanassova <jasminaata@gmail.com>
2014-01-12 16:31:56 +0000

    Precompiling assets

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,6 @@ module ShareMe
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    # config.assets.precompile = ['*.js', '*.css', '*.css.erb']
+    config.assets.precompile = ['*.js', '*.css', '*.css.erb']
   end
 end
